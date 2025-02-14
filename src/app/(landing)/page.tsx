7b0c24b7e7a6c374d3cb74f946057b3ded19af70
Kotalos <kotalosyt@gmail.com>
2025-02-14 19:59:36 +0100

    chore: format

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,7 @@
 export default function LandingPage() {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center gap-8">
-      <div className="w-50 flex flex-col items-start justify-center space-y-4 px-8 pt-16">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-8">
+      <div className="flex w-50 flex-col items-start justify-center space-y-4 px-8 pt-16">
         <h1 className="text-4xl font-extrabold">Software Engineer</h1>
         <p>
           Full stack developer with startup experience (engineer 0 to 1) and
@@ -9,17 +9,15 @@ export default function LandingPage() {
           contribute to challenging projects while expanding my technical
           expertise.
         </p>
-        <div className="bg-white shadow-m rounded-lg p-6 space-y-4 border border-gray-200">
-          <h2 className="text-2xl font-bold">
-            Looking for a 5 months Internship
-          </h2>
+        <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-6 shadow-m">
+          <h2 className="text-2xl font-bold">Looking for a 5 months Internship</h2>
           <p className="text-gray-600">
             Mobility Europe and North America. Availability April to August
           </p>
           <div className="flex space-x-4">
             <a
               href="https://www.linkedin.com/in/thomas-los/"
-              className="px-4 py-2 bg-slate-950 text-white rounded hover:bg-slate-200 hover:text-slate-950"
+              className="rounded bg-slate-950 px-4 py-2 text-white hover:bg-slate-200 hover:text-slate-950"
             >
               Contact
             </a>
@@ -27,7 +25,7 @@ export default function LandingPage() {
               href="https://www.linkedin.com/in/thomas-los/"
               target="_blank"
               rel="noopener noreferrer"
-              className="px-4 py-2 text-slate-950 border border-2 border-slate-950 rounded hover:border-slate-200 hover:bg-slate-200"
+              className="rounded border border-2 border-slate-950 px-4 py-2 text-slate-950 hover:border-slate-200 hover:bg-slate-200"
             >
               IN
             </a>
